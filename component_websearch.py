be04cb82f504528eefa67e84918e3dea5b8db923
Sieve Lau <sievelau@gmail.com>
2025-04-27 19:53:10 +0800

    fake_search用于测试

--- component_websearch.py
+++ component_websearch.py
@@ -1,6 +1,34 @@
 from langchain_core.documents import Document
 from init_env import env
 
+def _fake_search(query: str):
+    """Fake search function for testing purposes."""
+    import json
+    j = json.load(open("fake_search.json"))
+    documents: list[Document] = []
+
+    # collect first 5 urls from the search results and pass to tavily extract
+    for item in j['results']:
+        metadata = {
+            "source": item.get("url", ""),
+            "relevance": item.get("score", ""),
+            "title": item.get("title", "")
+        }
+        content = item.get("raw_content", None)
+        if content is None:
+            content = item.get("content", None)
+        if content is None:
+            continue
+        try:
+            document = Document(
+                page_content=content,
+                metadata=metadata
+            )
+            documents.append(document)
+        except Exception as e:
+            print(f"Error processing document: {e}")
+    return documents
+
 def search(query: str) -> list[Document]:
     """Search the web for realtime information."""
     TAVILY_KEY = env.get('TAVILY_KEY')
@@ -39,5 +67,6 @@ def search(query: str) -> list[Document]:
     return documents
 
 if __name__ == "__main__":
-    documents = search("HK weather today")
+    # documents = search("HK weather today")
+    documents = _fake_search("capital France")
     print(documents)
