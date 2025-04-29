9f945e627137de2ec3f22bbb4537a94e42a0dac5
Sieve Lau <sievelau@gmail.com>
2025-04-29 23:19:56 +0800

    remove Chroma; add add_date to database meta

--- globalsource.py
+++ globalsource.py
@@ -4,18 +4,18 @@ from init_env import env
 from langchain_openai import OpenAIEmbeddings
 from transformers import AutoTokenizer
 from langchain_text_splitters import TokenTextSplitter
-from langchain_chroma import Chroma
 from langchain.chat_models import init_chat_model
 from openai import OpenAI
 import redis
 from langchain_redis import RedisConfig, RedisVectorStore
+from datetime import datetime
 
 
 class _Resource:
     GLOBAL_VOLATILE_INDEX_NAME = "volatile_storage"
     MINIMUM_SCHEMA = [
         {"name": "source", "type": "text"},
-        {"name": "title", "type": "text"}
+        {"name": "title", "type": "text"},
     ]
 
     def __init__(self):
@@ -50,8 +50,8 @@ class _Resource:
         self.main_model_tokenizer = AutoTokenizer.from_pretrained(
             env.get("LOCAL_TOKENIZER_PATH"), local_files_only=True
         )
-        
-        self._redis_client = redis.from_url(env.get('REDIS_URL'))
+
+        self._redis_client = redis.from_url(env.get("REDIS_URL"))
         # on this machine, redis will be up after a few seconds
         while not self._redis_client.ping():
             sleep(5)
@@ -61,11 +61,14 @@ class _Resource:
             redis_client=self._redis_client,
             metadata_schema=[
                 {"name": "title", "type": "text"},
-                {"name": "source", "type": "text"}
+                {"name": "source", "type": "text"},
+                {"name": "add_date", "type": "text"},
             ],
         )
 
-        self.vector_store = RedisVectorStore(self.embed, config=config, ttl=7*24*3600)
+        self.vector_store = RedisVectorStore(
+            self.embed, config=config, ttl=7 * 24 * 3600
+        )
 
     def get_embed(self):
         return self.embed
@@ -75,19 +78,33 @@ class _Resource:
 
     def get_vector_store(self):
         return self.vector_store
-    
+
     def add_documents(self, documents):
+        now = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S UTC%z")
+        for doc in documents:
+            if doc.metadata is not None:
+                doc.metadata["add_date"] = now
+            else:
+                doc.metadata = {
+                    "title": "unknown",
+                    "source": "unknown",
+                    "add_date": now,
+                }
         all_splits = self.text_splitter.split_documents(documents)
+
         ids = self.vector_store.add_documents(all_splits)
         return ids
+
     def search_documents(self, query, k=2):
         docs, scores = zip(*self.vector_store.similarity_search_with_score(query, k))
         for doc, score in zip(docs, scores):
             doc.metadata["score"] = score
         return docs
-    
+
     def get_retriver(self, k: int):
-        return self.vector_store.as_retriever(search_type="similarity", search_kwargs={"k": k})
+        return self.vector_store.as_retriever(
+            search_type="similarity", search_kwargs={"k": k}
+        )
 
     def get_main_model_tokenizer(self):
         return self.main_model_tokenizer
@@ -97,5 +114,6 @@ class _Resource:
 
     def get_openai_model(self):
         return self.openai_model
+
 
 resource = _Resource()
