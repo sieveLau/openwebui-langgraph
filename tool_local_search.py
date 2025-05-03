f9ab3876d8d07f39096e7886aaee1c15f7cd1e33
Sieve Lau <sievelau@gmail.com>
2025-05-03 19:37:45 +0800

    用parameter来引导模型生成参数，取消内置生成

--- tool_local_search.py
+++ tool_local_search.py
@@ -1,11 +1,10 @@
 from globalsource import resource
+from typing import Annotated
 from langchain_core.tools import tool
-from tool_search import generate_query
 
-@tool("search_knowledge")
-def search_local_knowledge(user_question: str):
-    """Search the local knowledge database. This local database contains cached search results from the past seven days. The AI should prioritize querying this database for relevant results before making requests to the online search tool."""
-    query = generate_query(user_question)
+@tool("search_knowledge_database")
+def search_local_knowledge(query: Annotated[str, "Keywords to search the database"]) -> str:
+    """Search the local knowledge database. This local database contains cached online search results from the past seven days. The AI should prioritize querying this database for relevant results before making requests to the online search tool. If user mentions time, you MUST use other tools to get time before using this tool."""
     constructor = ""
     for i, doc in enumerate(resource.search_documents(query), start=1):
         constructor += """## Source ID: {}
@@ -14,11 +13,9 @@ Title: {}
 
 URL: {}
 
-Relevance Score: {}
-
 Added Date: {}
 
 {}
 
-""".format(i, doc.metadata['title'], doc.metadata['source'], doc.metadata['score'], doc.metadata['add_date'], doc.page_content)
+""".format(i, doc.metadata['title'], doc.metadata['source'], doc.metadata['add_date'], doc.page_content)
     return constructor
