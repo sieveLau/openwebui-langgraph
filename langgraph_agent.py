0507d57e0c5160e8cbc08fad91ebed13974cbce4
Sieve Lau <sievelau@gmail.com>
2025-05-04 16:55:00 +0800

    自己实现合适的trim message方法

--- langgraph_agent.py
+++ langgraph_agent.py
@@ -8,14 +8,13 @@ from tool_search import web_search_returning_string as web_search
 from tool_local_search import search_local_knowledge
 import tool_time
 from langgraph.prebuilt import ToolNode
-from langchain_core.messages import trim_messages, RemoveMessage
+from langchain_core.messages import RemoveMessage, ToolCall
+from trim_msg import trim_messages
 
 # ============ Define state for application BGN ============
 
-
 class State(TypedDict):
     messages: Annotated[list, add_messages]
-
 
 # ============ Define state for application END ============
 
@@ -30,31 +29,39 @@ model_with_tools = resource.get_main_model().bind_tools(tools)
 
 # TODO: replace with summary node
 def trim_message_list(state: State):
-    original_message_count = len(state["messages"])
-    trimmed = trim_messages(
-        state["messages"],
-        token_counter=tiktoken_counter,
-        strategy="last",
-        max_tokens=16 * 1024 - resource.MIN_NUM_PREDICT,
-        # this is essential, to keep at least one human message
-        end_on="human",
-        include_system=True,
-    )
-    trimmed_message_count = len(trimmed)
-    remove_n = original_message_count - trimmed_message_count
-    if remove_n == 0:
-        return {"messages": []}
+    messages = state["messages"]
+    trimmed = trim_messages(messages, 16 * 1024 - resource.MIN_NUM_PREDICT, tiktoken_counter)
     trimmed_ids = set([message.id for message in trimmed])
-    original_ids = set([message.id for message in state["messages"]])
+    original_ids = set([message.id for message in messages])
     removed_ids = original_ids - trimmed_ids
     return {"messages": [RemoveMessage(id=id) for id in removed_ids]}
 
+def fixing(state: State):
+    messages = state["messages"]
+    last_message = messages[-1]
+    invalid_call = last_message.invalid_tool_calls[0]
+    
+    
+    new_tool_call = ToolCall(
+        name=invalid_call['name'],
+        args={},
+        id=invalid_call['id']
+    )
+    last_message.tool_calls=new_tool_call,
+    last_message.invalid_tool_calls=[]
+    print(last_message)
+    return {"messages": [last_message]}
 
 def should_continue(state: State):
     messages = state["messages"]
     last_message = messages[-1]
+    print(last_message)
     if last_message.tool_calls:
         return "tools"
+    ## Reroute to a fixing node to manipulate the record to create a valid tool call.
+    ## TODO: detect true error
+    elif len(last_message.invalid_tool_calls) > 0:
+        return "fixing"
     return END
 
 
@@ -69,10 +76,16 @@ workflow = StateGraph(State)
 workflow.add_node("agent", call_model)
 workflow.add_node("tools", tool_node)
 workflow.add_node("trim", trim_message_list)
+workflow.add_node("fixing", fixing)
 
 workflow.add_edge(START, "trim")
 workflow.add_edge("trim", "agent")
-workflow.add_conditional_edges("agent", should_continue, ["tools", END])
+workflow.add_conditional_edges("agent", 
+                               # Next, we pass in the function that will determine which node is called next.
+                               should_continue, 
+                               # Next, we pass in the path map - all the possible nodes this edge could go to
+                               ["tools", "fixing", END])
 workflow.add_edge("tools", "trim")
+workflow.add_edge("fixing", "tools")
 
 app = workflow.compile()
